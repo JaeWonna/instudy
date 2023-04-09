ff0d7f72c34429c7bf962f5e863e9ed30b7e7064
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-04-09 17:33:30 +0900

    [back]FEAT: lombok 의존성 추가 - @Getter @Setter

--- instudy/src/main/java/instudy/instudy/domain/User.java
+++ instudy/src/main/java/instudy/instudy/domain/User.java
@@ -1,10 +1,14 @@
 package instudy.instudy.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Getter @Setter
 @Entity(name = "user") // 테이블 명 user
 public class User {
 
@@ -30,45 +34,5 @@ public class User {
 
     public User() {
 
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
