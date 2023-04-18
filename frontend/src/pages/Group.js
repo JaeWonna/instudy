cb26d18c568087a5a7a2cf9a263f444a7947d0a9
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-04-18 23:15:45 +0900

    [front] FEAT: 그룹페이지에서 세션에 저장된 로그인 정보를 가져오고 props로 모달에 로그인 유저 이름 넘김

--- frontend/src/pages/Group.js
+++ frontend/src/pages/Group.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import {Container, Row, Col, Button} from 'react-bootstrap';
-import groupMember from '../img/groupMember.jpg'; 
+import groupMember from '../img/groupMember.jpg';
 import { Link } from "react-router-dom";
 import GroupCreateModal from "../components/group/GroupCreateModal";
 import '../css/Group.css';
@@ -8,15 +8,26 @@ import '../css/Group.css';
 
 const Group = () => {
     const [group, setGroup] = useState([]);
-
+    const [loginUser, setLoginUser] = useState({});
     useEffect(()=> {
         const groupdata = [
             {id: 1, content: '정보처리기사 필기 스터디', link: '/GroupMainView/1'},
-            {id: 2, content: '스프링 스터디', link: '/GroupMainView/2'}, 
-            {id: 3, content: '리액트 스터디', link: '/GroupMainView/3'}, 
-               ];
+            {id: 2, content: '스프링 스터디', link: '/GroupMainView/2'},
+            {id: 3, content: '리액트 스터디', link: '/GroupMainView/3'},
+        ];
 
         setGroup([...groupdata]);
+
+
+        const storedUser = sessionStorage.getItem("loginUser");
+        if (storedUser) { // 세션에 로그인한 유저가 저장되었을 때
+            const parsedUser = JSON.parse(storedUser).data;
+            setLoginUser(parsedUser);
+            // console.log(loginUser.user_name)
+        } else { // 세션에 저장된 유저가 null일 때
+
+        }
+
     }, []);
 
     const imgStyle = {
@@ -31,33 +42,33 @@ const Group = () => {
 
     return (
         <>
-        <Container>
-        {
-            group.map(group => (
-                <>
-                {/* <Container> */}
-                <Row>{" "}</Row>
-                <Row><Link to={`/group/${group.id}`} key={group.id}>
-                    {group.content}
-                </Link></Row>
-                <hr/>
-                {/* </Container> */}
-                </>
-            ))
-        }
-        <div class="row">
-        <div class="col-md-12 col-lg-6"></div>
+            <Container>
+                {
+                    group.map(group => (
+                        <>
+                            {/* <Container> */}
+                            <Row>{" "}</Row>
+                            <Row><Link to={`/group/${group.id}`} key={group.id}>
+                                {group.content}
+                            </Link></Row>
+                            <hr/>
+                            {/* </Container> */}
+                        </>
+                    ))
+                }
+                <div class="row">
+                    <div class="col-md-12 col-lg-6"></div>
 
-        <div class="d-flex justify-content-end align-items-end">
+                    <div class="d-flex justify-content-end align-items-end">
 
-<GroupCreateModal />
+                        <GroupCreateModal manager={loginUser.user_name} />
 
-</div>
-        </div>
-       
-        </Container>
-        
-        {/* <button type="button" class="btn btn-primary btn-floating btn-lg" onClick={showModal}>
+                    </div>
+                </div>
+
+            </Container>
+
+            {/* <button type="button" class="btn btn-primary btn-floating btn-lg" onClick={showModal}>
   <i class="fab fa-airbnb fa-lg pe-none"></i>
 </button>
 {modalOpen &&  */}
