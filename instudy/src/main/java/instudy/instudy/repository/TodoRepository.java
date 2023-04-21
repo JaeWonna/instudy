ba03ab03e2cb776929e148cf85ced45817a1d0e6
JaeWonna <highjae09@gmail.com>
2023-04-21 15:38:42 +0900

    [back]FEAT : Todo리포지토리 작성

---
+++ instudy/src/main/java/instudy/instudy/repository/TodoRepository.java
@@ -0,0 +1,15 @@
+package instudy.instudy.repository;
+
+import instudy.instudy.domain.StudyGroup;
+import instudy.instudy.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    Todo save(Todo todo);
+
+}
