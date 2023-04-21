ca82549c9a9a6ee7c9c9aa481e8ce6e194106302
JaeWonna <highjae09@gmail.com>
2023-04-21 15:09:33 +0900

    [back]FEAT : Todo도메인 변경 및 간략화

--- instudy/src/main/java/instudy/instudy/domain/Todo.java
+++ instudy/src/main/java/instudy/instudy/domain/Todo.java
@@ -13,22 +13,29 @@ import static javax.persistence.FetchType.LAZY;
 public class Todo {
 
     @Id @GeneratedValue
-    @Column(name = "todo_id")
     private Long id;
-
-    private String title;
-    private String description;
-
-    @Enumerated(EnumType.STRING)
-    private StudyStatus status; // 공부 상태
-
-    private LocalDateTime studydate; // 공부한 시간 날짜 측정변수
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
+    // 여기부터 새로 추가한내용
+    private String todo_text;
+
+    private String status;
+
+    public Todo(String todo_text, String status) {
+        this.todo_text = todo_text;
+        this.status = status;
+    }
+
+    public Todo() {
+
+    }
+
 }
+
+// 커밋용
 
 //
 //    id (기본키) Long
@@ -38,3 +45,20 @@ public class Todo {
 //        status : Status (시작전 / 진행중 / 완료)
 //        studydate : LocalDateTime (시간날짜측정변수)
 
+
+//    < 원래 있었던 Todo내용 >
+//    @Id @GeneratedValue
+//    @Column(name = "todo_id")
+//    private Long id;
+//
+//    private String title;
+//    private String description;
+//
+//    @Enumerated(EnumType.STRING)
+//    private StudyStatus status; // 공부 상태
+//
+//    private LocalDateTime studydate; // 공부한 시간 날짜 측정변수
+//
+//    @ManyToOne(fetch = LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
