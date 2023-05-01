a8a00e85002a6d78d6a7d5f4088d2fb2053e706e
hido02 <bestdohee@gmail.com>
2023-05-01 17:36:07 +0900

    [front]FIX: Context 범위 헤더에만 적용하고 있던 문제 해결, App 전체에 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,4 @@
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
-import React, { useState, createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { Outlet } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
@@ -33,34 +8,25 @@ import Footer from './components/Footer';
 import GroupBottomNav from './components/group/GroupBottomNav';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-  const MyContext = React.createContext({ isLoggedIn: true });
+    const MyContext = React.createContext({ isLoggedIn: true });
 
-  const path = window.location.pathname;
-
-  console.log(path)
-
-  return (
-    <>
-<MyContext.Provider value={{ isLoggedIn: true }}>
-    <Header MyContext={MyContext} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
-    </MyContext.Provider>
-    <Outlet />
-    {/* <BottomNavBar /> */}
-    <Footer />
-    {/* {path === '/group' ? <GroupBottomNav /> : <BottomNav /> } */}
-    {/* <BrowserRouter> */}
-    <Routes>
-      <Route  path="/" element={<BottomNav />} />
-        <Route  path="/profile" element={<BottomNav />} />
-        <Route  path="/group" element={<BottomNav />} />
-      <Route  path="/group/:id" element={<GroupBottomNav />} />
-    </Routes>
-    {/* </BrowserRouter> */}
-    
-    </>
-  );
+    return (
+        <MyContext.Provider value={{ isLoggedIn: true }}>
+            <div MyContext={MyContext} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}>
+                <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+                <Outlet />
+                <Footer />
+                <Routes>
+                    <Route  path="/" element={<BottomNav />} />
+                    <Route  path="/profile" element={<BottomNav />} />
+                    <Route  path="/group" element={<BottomNav />} />
+                    <Route  path="/group/:id" element={<GroupBottomNav />} />
+                </Routes>
+            </div>
+        </MyContext.Provider>
+    );
 }
 
 export default App;
