d87eb74d0889e387e1bca1cd2f6f00f16693e3a8
hido02 <bestdohee@gmail.com>
2023-05-02 18:00:05 +0900

    [front]FEAT: 메인화면 반응형 페이드아웃 구현중 (정렬 필요)

---
+++ frontend/src/components/common/layouts/DesktopMain.js
@@ -0,0 +1,91 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import HomeCard from "../../Home/HomeCard";
+import React, { useState, useEffect } from "react";
+import SignIn from '../../../containers/sign/sign_in/SignIn';
+import mainTime from '../../../img/mainTime.jpg';
+import mainGrass from '../../../img/mainGrass.png';
+import mainGoodJob from '../../../img/mainGoodJob.jpg';
+
+function DesktopMain() {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(()=> {
+        const menudata = [
+            {title: "그룹", link: "/group"},
+            {title: "프로필", link: "/profile"}
+        ];
+
+        setMenu([...menudata]);
+
+        console.log(menu);
+    }, []);
+
+    const textStyle = {
+        textAlign: 'center'
+    }
+
+    const imgStyle = {
+        width: '400px',
+    }
+
+    const backgroundStyle = {
+        backgroundImage: `url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp')`,
+        height: '2000px'
+    }
+
+    const maskStyle = {
+        backgroundColor: 'rgba(0, 0, 0, 0.6)'
+    }
+
+    const descStyle = {
+        margin: '50px 0'
+    }
+
+    return (
+        <>
+            <div class="p-5 text-center bg-image" style={backgroundStyle}>
+                <div class="mask" style={maskStyle}>
+                    <div class="row">
+                        <div class="col-md-3"></div>
+                        <div class="col-md-6">
+                            <div class="d-flex justify-content-center align-items-center" style={{margin: '20px 0'}}>
+                                <div class="text-white">
+                                    <h1 class="mb-3"><b>Instudy는</b></h1>
+                                    <h4 class="mb-3">스터디 그룹 관리 및 그룹별 공유 일기 플랫폼입니다</h4>
+                                    <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+                                </div>
+                            </div>
+                            <div class="card" style={{margin: '20px 0'}}>
+                                <div class="card-body" style={textStyle}>
+                                    <h5 class="card-title" ><b>무의미한 시간 조작은 No</b></h5>
+                                    <img src={mainTime} alt="Generic placeholder" style={imgStyle} />
+                                    <p class="card-text" style={descStyle}>공부는 양보다 질, 어쩌구</p>
+                                    <hr />
+                                    <h5 class="card-title"><b>잔디 심기로 학습 성취를 얻으세요</b></h5>
+                                    <img src={mainGrass} alt="Generic placeholder" style={imgStyle} />
+                                    <p class="card-text" style={descStyle}>재밌음 어쩌구</p>
+                                    <hr />
+                                    <h5 class="card-title"><b>그룹원 간의 상호 '인정'을 통해 객관적인 피드백을 받으세요</b></h5>
+                                    <img src={mainGoodJob} alt="Generic placeholder" style={imgStyle} />
+                                    <p class="card-text" style={descStyle}>실질적이고 능동적인 학습관리 어쩌구</p>
+                                    <hr />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3"></div>
+                    </div>
+
+
+
+
+
+
+                </div>
+            </div>
+
+        </>
+
+    )
+}
+
+export default DesktopMain;
--- frontend/src/components/common/layouts/Fadeout.js
+++ frontend/src/components/common/layouts/Fadeout.js
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import {Grid, Typography} from "@mui/material";
+import {Grid, Typography, Box} from "@mui/material";
+import { useMediaQuery } from 'react-responsive';
 
 function Fadeout() {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 1224px)'
+    });
+
     useEffect(() => {
         const timeoutId = setTimeout(() => {
             window.location.href = '/main';
@@ -11,12 +16,18 @@ function Fadeout() {
         return () => clearTimeout(timeoutId);
     }, []);
 
-    return <div>
-        <Grid container alignItems="center">
-        <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: '100px'}}/>
-        <Typography variant="h1">Instudy</Typography>
-    </Grid>
-    </div>
+    return (
+        <div>
+            <Grid container justifyContent="center">
+                <Grid item xs={12} sm={6} md={4}>
+                    <div>
+                        <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: '100px'}}/>
+                        <Typography variant="h1">Instudy</Typography>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default Fadeout;
