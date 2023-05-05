663460f3853043c6a502127111f999853a6ccb25
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2023-05-05 11:24:10 +0900

    [back] feat: TodoController 상태 업데이트 메소드 추가 및 StudyStatus 넘겨주는 방식 변경

--- instudy/src/main/java/instudy/instudy/controller/TodoController.java
+++ instudy/src/main/java/instudy/instudy/controller/TodoController.java
@@ -1,5 +1,6 @@
 package instudy.instudy.controller;
 
+import instudy.instudy.domain.StudyStatus;
 import instudy.instudy.domain.Todo;
 import instudy.instudy.domain.User;
 import instudy.instudy.service.TodoService;
@@ -19,11 +20,11 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    @RequestMapping(value = "/todo", method = RequestMethod.POST)
+    @RequestMapping(value = "/todo", method = RequestMethod.POST)   // todo create
     public boolean postCreateForm(@RequestBody Map<String, String> paramMap) {
         System.out.println(paramMap);
         String todo_text = paramMap.get("todo_text");
-        Todo newTodo = new Todo(todo_text);
+        Todo newTodo = new Todo(todo_text, StudyStatus.READY);
         return todoService.join(newTodo);    //정상적으로 저장되면 true return 합니다!!
     }
 
@@ -39,5 +40,13 @@ public class TodoController {
         Todo deleteTodo = new Todo(todo_text);
         todoService.deleteTodo(deleteTodo);
         return "delete";
+    }
+
+    @RequestMapping(value = "/todo/updateStatus", method = RequestMethod.POST)
+    public String postUpdateStatus(@RequestBody Map<String, String> paramMap) {
+        String todo_text = paramMap.get("todo_text");
+        Todo finishTodo = new Todo(todo_text, StudyStatus.FINISH);
+        todoService.updateStatus(finishTodo);
+        return "updateStatus";
     }
 }
