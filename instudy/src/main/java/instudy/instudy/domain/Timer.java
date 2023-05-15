a259085a17603f101cbb1451f9247f0817e30104
JaeWonna <highjae09@gmail.com>
2023-05-15 14:56:18 +0900

    [back]FEAT : 연관관계 때문에 오류로 todo리스트 안뜨는것 해결

---
+++ instudy/src/main/java/instudy/instudy/domain/Timer.java
@@ -0,0 +1,24 @@
+package instudy.instudy.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "timer")
+@Getter @Setter
+public class Timer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long timerId;
+
+    private int count = 0;
+
+    private int hour, minute, second; // 시분초, 이걸로 카운트 다운 및 업
+
+    @Enumerated(EnumType.STRING)
+    private TimerStatus timerStatus; // run, stop
+
+    private LocalDateTime localDateTime; // 현재 시간!
+}
---
+++ instudy/src/main/java/instudy/instudy/domain/TimerStatus.java
@@ -0,0 +1,6 @@
+package instudy.instudy.domain;
+
+public enum TimerStatus {
+
+    RUN, STOP // 스톱워치 동작중, 동작멈춰!
+}
