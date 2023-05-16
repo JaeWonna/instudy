3e747a390b1a3b6b9aedd71c49b3017a13dd518d
hido02 <bestdohee@gmail.com>
2023-05-16 18:25:52 +0900

    [front]DESIGN: 그룹 생성/검색 아이콘 변경

--- frontend/src/components/group/GroupCreateModal.js
+++ frontend/src/components/group/GroupCreateModal.js
@@ -15,20 +15,20 @@ import ModalStaticBackdrop from "../../components/common/modal/ModalStaticBackdr
 import GroupFilterModal from "./finder/GroupFilterModal";
 
 const GroupCreateModal = (props) => {
-  const closeModal = () => {
-    props.setModalOpen(false);
-  }
+    const closeModal = () => {
+        props.setModalOpen(false);
+    }
 
-  const [groupName, setGroupName] = useState("");
-  const [description, setDescription] = useState("");
+    const [groupName, setGroupName] = useState("");
+    const [description, setDescription] = useState("");
 
-  const handleInputChange1 = (e) => {
-      setGroupName(e.target.value);
-  }
+    const handleInputChange1 = (e) => {
+        setGroupName(e.target.value);
+    }
 
-  const handleInputChange2 = (e) => {
-      setDescription(e.target.value);
-  }
+    const handleInputChange2 = (e) => {
+        setDescription(e.target.value);
+    }
 
     const [selectedSkillTags, setSelectedSkillTags] = useState([]);
 
@@ -82,8 +82,8 @@ const GroupCreateModal = (props) => {
             });
     };
 
-  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-  const checkedIcon = <CheckBoxIcon fontSize="small" />;
+    const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+    const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
     const [groupFilterModalOpen, setGroupFilterModalOpen] = useState(false);
 
@@ -107,18 +107,20 @@ const GroupCreateModal = (props) => {
     }
 
     return (
-        <div>     
-<div class="row">
-        <div class="col-md-12 col-lg-6"></div>
+        <div>
+            <div class="row">
+                <div class="col-md-12 col-lg-6"></div>
 
-        <div class="d-flex justify-content-end align-items-end">
+                <div class="d-flex justify-content-end align-items-end">
 
-<Button onClick={() => setGroupFilterModalOpen(true)}>
-<FontAwesomeIcon icon={faSearch} />
-</Button>
+                    <div className="rounded-icon">
+                    <Button onClick={() => setGroupFilterModalOpen(true)}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </Button>
+                    </div>
 
-</div>
-        </div>
+                </div>
+            </div>
 
             <ModalStaticBackdrop
                 keepMounted
@@ -127,105 +129,105 @@ const GroupCreateModal = (props) => {
                 component={<GroupFilterModal filterGroup={filterGroup} resetGroups={resetGroups} setOpen={setGroupFilterModalOpen} />}
             />
 
-        <div class="row">
-        <div class="col-md-12 col-lg-6"></div>
+            <div class="row">
+                <div class="col-md-12 col-lg-6"></div>
 
-        <div class="d-flex justify-content-end align-items-end">
+                <div class="d-flex justify-content-end align-items-end">
 
-<div className="rounded-icon">
-<button style={null} data-mdb-toggle="modal" data-mdb-target="#exampleModal">
-<FontAwesomeIcon icon={faPlus} />
-</button>
-</div>
+                    <div className="rounded-icon">
+                        <Button style={null} data-mdb-toggle="modal" data-mdb-target="#exampleModal">
+                            <FontAwesomeIcon icon={faPlus} />
+                        </Button>
+                    </div>
 
-</div>
-        </div>
+                </div>
+            </div>
 
-          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-          <Typography variant='h5' my={1}>그룹 생성하기</Typography>
-        <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <Typography variant='h5' my={1}>그룹 생성하기</Typography>
+                            <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
 
-          <Box
-              component="form"
-              sx={{
-                  '& .MuiTextField-root': { m: 1, width: '90%' },
-              }}
-              noValidate
-              autoComplete="off"
-          >
-              <div>
-                  <TextField
-                      id="outlined-multiline-flexible"
-                      label="그룹 이름"
-                      multiline
-                      maxRows={4}
-                      value={groupName}
-                      onChange={handleInputChange1}
-                  />
+                            <Box
+                                component="form"
+                                sx={{
+                                    '& .MuiTextField-root': { m: 1, width: '90%' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <div>
+                                    <TextField
+                                        id="outlined-multiline-flexible"
+                                        label="그룹 이름"
+                                        multiline
+                                        maxRows={4}
+                                        value={groupName}
+                                        onChange={handleInputChange1}
+                                    />
 
-                  <TextField
-                      id="outlined-multiline-static"
-                      label="그룹 설명"
-                      multiline
-                      rows={4}
-                      defaultValue="Default Value"
-                      value={description}
-                      onChange={handleInputChange2}
-                  />
-              </div>
+                                    <TextField
+                                        id="outlined-multiline-static"
+                                        label="그룹 설명"
+                                        multiline
+                                        rows={4}
+                                        defaultValue="Default Value"
+                                        value={description}
+                                        onChange={handleInputChange2}
+                                    />
+                                </div>
 
-              <b>공부 종류 키워드(projectStack)</b>
-              <Autocomplete
-                  multiple
-                  options={tags.tech}
-                  disableCloseOnSelect
-                  getOptionLabel={(option) => option.label}
-                  value={selectedSkillTags}
-                  onChange={(event, newValue) => {
-                      setSelectedSkillTags(newValue);
-                  }}
-                  renderOption={(props, option, { selected }) => (
-                      <li {...props}>
-                          <Checkbox
-                              icon={icon}
-                              checkedIcon={checkedIcon}
-                              style={{ marginRight: 8 }}
-                              checked={selected}
-                          />
-                          {option.label}
-                      </li>
-                  )}
-                  renderInput={(params) => (
-                      <TextField {...params} label="이 그룹에서 공부하려는 것은..." placeholder="키워드" />
-                  )}
-              />
-              <TextField
-                  id="outlined-select-currency"
-                  select
-                  label="Select"
-                  defaultValue="1"
-              >
-                  {currencies.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                      </MenuItem>
-                  ))}
-              </TextField>
-          </Box>
+                                <b>공부 종류 키워드(projectStack)</b>
+                                <Autocomplete
+                                    multiple
+                                    options={tags.tech}
+                                    disableCloseOnSelect
+                                    getOptionLabel={(option) => option.label}
+                                    value={selectedSkillTags}
+                                    onChange={(event, newValue) => {
+                                        setSelectedSkillTags(newValue);
+                                    }}
+                                    renderOption={(props, option, { selected }) => (
+                                        <li {...props}>
+                                            <Checkbox
+                                                icon={icon}
+                                                checkedIcon={checkedIcon}
+                                                style={{ marginRight: 8 }}
+                                                checked={selected}
+                                            />
+                                            {option.label}
+                                        </li>
+                                    )}
+                                    renderInput={(params) => (
+                                        <TextField {...params} label="이 그룹에서 공부하려는 것은..." placeholder="키워드" />
+                                    )}
+                                />
+                                <TextField
+                                    id="outlined-select-currency"
+                                    select
+                                    label="Select"
+                                    defaultValue="1"
+                                >
+                                    {currencies.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </Box>
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" onClick={() => createGroup()}>그룹 생성</button>
-      </div>
-    </div>
-  </div>
-</div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" onClick={() => createGroup()}>그룹 생성</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
