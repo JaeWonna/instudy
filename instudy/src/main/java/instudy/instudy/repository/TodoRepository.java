24196afa507873dd84efd16139de1a5e8df16824
JaeWonna <highjae09@gmail.com>
2023-05-19 00:49:05 +0900

    [back]FEAT : findByUserId Repository

--- instudy/src/main/java/instudy/instudy/repository/TodoRepository.java
+++ instudy/src/main/java/instudy/instudy/repository/TodoRepository.java
@@ -3,9 +3,15 @@ package instudy.instudy.repository;
 import instudy.instudy.domain.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     Todo save(Todo todo);
 
     Todo findByTodoText(String todo_text);
+
+    
+    // 유저 아이디로 투두 리스트로 반환받기 -> 이게 되는가? 안될것같은데?
+    List<Todo> findByUserId(String userId);
 }
