cc6015a0984599d419f1ede0f6e22aa74b4de6c0
JaeWonna <highjae09@gmail.com>
2023-05-29 17:31:31 +0900

    [back]FEAT : main화면 변경, 홈화면에 어울리는 사진 png로 업로드, 배경화면 사진 변경 해야됨

--- frontend/src/components/common/layouts/DesktopMain.js
+++ frontend/src/components/common/layouts/DesktopMain.js
@@ -6,6 +6,15 @@ import mainTime from '../../../img/mainTime.jpg';
 import mainGrass from '../../../img/mainGrass.png';
 import mainGoodJob from '../../../img/mainGoodJob.jpg';
 
+import mainBackGround from '../../../img/mainBackGround.png';
+import mainPic1 from '../../../img/mainPic1.png';
+import mainPic2 from '../../../img/mainPic2.png';
+import mainPic3 from '../../../img/mainPic3.png';
+import mainBluePic1 from '../../../img/mainBluePic1.png';
+import mainBluePic2 from '../../../img/mainBluePic2.png';
+import mainBluePic3 from '../../../img/mainBluePic3.png';
+
+
 function DesktopMain() {
     const [menu, setMenu] = useState([]);
 
@@ -50,25 +59,35 @@ function DesktopMain() {
                         <div class="col-md-6">
                             <div class="d-flex justify-content-center align-items-center" style={{margin: '20px 0'}}>
                                 <div class="text-white">
-                                    <h1 class="mb-3"><b>Instudy는</b></h1>
-                                    <h4 class="mb-3">스터디 그룹 관리 및 그룹별 공유 일기 플랫폼입니다</h4>
+                                    <h1 class="mb-3"><b>Instudy 에서 함께 공부해요!</b></h1>
+                                    <h4 class="mb-3"></h4>
                                     <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
                                 </div>
                             </div>
                             <div class="card" style={{margin: '20px 0'}}>
                                 <div class="card-body" style={textStyle}>
-                                    <h5 class="card-title" ><b>무의미한 시간 조작은 No</b></h5>
-                                    <img src={mainTime} alt="Generic placeholder" style={imgStyle} />
-                                    <p class="card-text" style={descStyle}>공부는 양보다 질, 어쩌구</p>
+                                    <h5 class="card-title" ><b>Instudy! 공식 인스타그램 OPEN!
+                                    </b></h5>
+                                    <img src={mainPic1} alt="Generic placeholder" style={imgStyle} />
+                                    <p class="card-text" style={descStyle}>지금 팔로우하고 다양한 소식을 만나보세요👋</p>
                                     <hr />
-                                    <h5 class="card-title"><b>잔디 심기로 학습 성취를 얻으세요</b></h5>
-                                    <img src={mainGrass} alt="Generic placeholder" style={imgStyle} />
-                                    <p class="card-text" style={descStyle}>재밌음 어쩌구</p>
+                                    <h5 class="card-title"><b>스터디를 찾는 가장 쉬운 방법
+                                    </b></h5>
+                                    <img src={mainPic2} alt="Generic placeholder" style={imgStyle} />
+                                    <p class="card-text" style={descStyle}>Instudy에서 함께할 팀원을 찾으세요🔍 </p>
                                     <hr />
-                                    <h5 class="card-title"><b>그룹원 간의 상호 '인정'을 통해 객관적인 피드백을 받으세요</b></h5>
-                                    <img src={mainGoodJob} alt="Generic placeholder" style={imgStyle} />
-                                    <p class="card-text" style={descStyle}>실질적이고 능동적인 학습관리 어쩌구</p>
+                                    <h5 class="card-title"><b>Instudy!는 여러분들의 스터디를 도와줍니다
+                                    </b></h5>
+                                    <img src={mainPic3} alt="Generic placeholder" style={imgStyle} />
+                                    <p class="card-text" style={descStyle}>Instudy에서 꿈과 목표를 이루세요🙏</p>
                                     <hr />
+                                    <h5 className="card-title"><b>👀Instudy! 이런 사람들에게 필요해요
+                                    </b></h5>
+                                    <p className="card-text" style={descStyle}>✅새로운 스터디를 시작해보고 싶으신 분</p>
+                                    <p className="card-text" style={descStyle}>✅발걸음을 같이 맞춰 나갈 스터디원을 찾고 계신 분</p>
+                                    <p className="card-text" style={descStyle}>✅공부에 대한 의무감 대신 성취감을 얻고 싶으신 분</p>
+
+                                    <hr/>
                                 </div>
                             </div>
                         </div>
