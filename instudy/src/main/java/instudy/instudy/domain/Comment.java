6a3dcefae7642d38534b2db0207c8762da8351c8
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-06-06 19:33:36 +0900

    [back] FEAT: 피드 댓글 Comment 도메인 추가

---
+++ instudy/src/main/java/instudy/instudy/domain/Comment.java
@@ -0,0 +1,32 @@
+package instudy.instudy.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "comment") // 테이블 명 feed
+@Getter
+@Setter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column
+    private String userId;
+    @Column
+    private String feedId;
+    @Column
+    private String comment;
+
+    public Comment(String userId, String feedId, String comment) {
+        this.userId = userId;
+        this.feedId = feedId;
+        this.comment = comment;
+    }
+
+    public Comment() {
+
+    }
+}
